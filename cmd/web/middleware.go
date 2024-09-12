146df524fbbb9fe2c46608c08ca32fc90140364c
adulmaev <dulmaev.andrei@gmail.com>
2024-09-12 17:04:32 +0500

    update sessions without pointer

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -146,7 +146,7 @@ func (app *application) sessionMiddleware(next http.Handler) http.Handler {
 func (app *application) authenticate(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		sess := app.SessionFromContext(r)
-		id, ok := (*sess).Get(AuthenticatedUserID).(int)
+		id, ok := sess.Get(AuthenticatedUserID).(int)
 		if !ok || id == 0 {
 			next.ServeHTTP(w, r)
 			return
