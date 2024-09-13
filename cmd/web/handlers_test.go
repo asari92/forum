f7a32654ba9ecfd70d92bb7ce42d0c1e63501d57
adulmaev <dulmaev.andrei@gmail.com>
2024-09-13 19:07:01 +0500

    add TestPostCreate

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -200,3 +200,38 @@ func TestUserSignup(t *testing.T) {
 		})
 	}
 }
+
+func TestPostCreate(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	t.Run("Unauthenticated", func(t *testing.T) {
+		code, headers, _ := ts.get(t, "/post/create")
+
+		assert.Equal(t, code, http.StatusSeeOther)
+		assert.Equal(t, headers.Get("Location"), "/user/login")
+	})
+
+	t.Run("Authenticated", func(t *testing.T) {
+		// Make a GET /user/login request and extract the CSRF token from the
+		// response.
+		_, _, body := ts.get(t, "/user/login")
+		csrfToken := extractCSRFToken(t, body)
+
+		// Make a POST /user/login request using the extracted CSRF token and
+		// credentials from our the mock user model.
+		form := url.Values{}
+		form.Add("email", "alice@example.com")
+		form.Add("password", "pa$$word")
+		form.Add("token", csrfToken)
+		ts.postForm(t, "/user/login", form)
+
+		// Then check that the authenticated user is shown the create snippet
+		// form.
+		code, _, body := ts.get(t, "/post/create")
+
+		assert.Equal(t, code, http.StatusOK)
+		assert.StringContains(t, body, "<form action='/post/create' method='POST'>")
+	})
+}
